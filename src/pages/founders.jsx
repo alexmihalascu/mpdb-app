f93221e6ab95723fa4f615b44671e7f7549a929e
BezneaMihai <mbeznea2002@gmail.com>
2023-01-14 20:40:38 +0200

    f

--- src/pages/founders.jsx
+++ src/pages/founders.jsx
@@ -3,19 +3,22 @@ const Founders = () =>{
     return (
         
         <>
-<div>
-    <h1>Alexandru Mihalașcu</h1>
-</div>
-<div>
-    <h2>Pencea Alexandru</h2>
-</div>
-<div>
-        <h3>Dinu Silviu-Ion</h3>
-</div>
-<div>
-    <h4>Beznea Mihai-Gabriel</h4>
-</div>
+<div className="popular container">  
+        <div className="column">
+           <h1>Alexandru Mihalașcu</h1> 
+        </div>
+        <div className="column">
+            <h2>Pencea Alexandru</h2>
+        </div>
+        <div className="column">
+            <h3>Dinu Silviu-Ion</h3>
+        </div>
+        <div className="column">
+            <h4>Beznea Mihai-Gabriel</h4> 
+        </div>       
+</div>  
         </>  
     )
 }
 export default Founders;
+
