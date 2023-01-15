9e468cc658fd73a0f8bd68ad8a4a1e0e36f66024
alexmihalascu <41302353+alexmihalascu@users.noreply.github.com>
2023-01-15 20:23:15 +0200

    deleted client directory

--- client/server/routes/db-config.js
+++
@@ -1,3 +0,0 @@
-const sql = require('mysql')
-const db = sql.createConnection({host:process.env.HOST, pass: process.env.PASS, database: process.env.DATABASE, user: process.env.USER})
-module.exports = db
