c87a6dcf136e0ee3fe50f5c3345a573b06b7b51a
alexmihalascu <41302353+alexmihalascu@users.noreply.github.com>
2023-01-16 23:43:12 +0200

    Delete db directory

--- db/database.js
+++
@@ -1,9 +0,0 @@
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('mpdb-db', (err) => {
-  if (err) {
-    console.error(err.message);
-  }
-  console.log('Connected to the SQlite database.');
-});
-
-module.exports = db;
