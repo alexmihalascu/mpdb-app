90b710ee3c85360078aa302f2833fc77bf9cdb95
Alex Mihalascu <alexmihalascu@gmail.com>
2023-01-18 00:58:32 +0200

    register update

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,29 +1,41 @@
 import React, { useState } from 'react';
 import "./register.css"
+import axios from 'axios';
 
 const Register = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-  
-    const handleSubmit = (e) => {
-      e.preventDefault();
-    }
-  
-    return (
-      <form className="center-form" onSubmit={handleSubmit}>
-        <label>
-          Username:
-          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-        </label>
-        <br />
-        <label>
-          Password:
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-        </label>
-        <br />
-        <button type="submit">Register</button>
-      </form>
-    );
-  };
+const [password, setPassword] = useState("");
+const [username, setUsername] = useState("");
 
-  export default Register;
+const handleSubmit = async (e) => {
+e.preventDefault();
+if (password !== username !== "") {
+try {
+await axios.post("http://127.0.0.1:5000/register", {
+username,
+password
+});
+window.location.href = "/";
+} catch (e) {
+console.log(e);
+}
+}
+};
+
+return (
+<form className="center-form" onSubmit={handleSubmit}>
+<label>
+Username:
+<input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+</label>
+<br />
+<label>
+Password:
+<input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+</label>
+<br />
+<button type="submit">Register</button>
+</form>
+);
+};
+
+export default Register;
